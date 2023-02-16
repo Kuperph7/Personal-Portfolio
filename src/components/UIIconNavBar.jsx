e92c7ca5ff7dc8b6421e6b4026ede209740831e4
Kuper <tomykup@gmail.com>
2023-02-16 11:10:52 -0300

    final portfolio

--- src/components/UIIconNavBar.jsx
+++ src/components/UIIconNavBar.jsx
@@ -2,15 +2,9 @@ import "../styles/layouts/UIIconNavBar.scss";
 import { useRef } from "react";
 
 export default function UIIconNavBar() {
-  const animationRef = useRef();
-
-  const animation = () => {
-    animationRef.current.classList.toggle("open");
-  };
-
   return (
     <div>
-      <div class="icon nav-icon-5" ref={animationRef} onClick={animation}>
+      <div class="icon nav-icon-5">
         <span></span>
         <span></span>
         <span></span>
